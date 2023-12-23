54e75a6ef1ae2cf861f6dfaa909f577f318a4f51
ryu <43063676+kojipy@users.noreply.github.com>
2023-12-23 21:52:05 +0900

    update

--- www/index.php
+++ www/index.php
@@ -11,9 +11,6 @@
         <section class="hero is-medium is-info is-bold">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK
-                    </h1>
                     <h2 class="subtitle">
                         Your local development environment
                     </h2>
